0286f0a9737808f881e91748f04c9c4a5a4c1145
Brina <brixiga1892@gmail.com>
2023-07-22 00:52:08 -0500

    agregando tareas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ListaTareasComponent } from './lista-tareas/lista-tareas.component';
+import { CommonModule } from '@angular/common';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaTareasComponent,
+  
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/lista-tareas/lista-tareas.component.ts
+++ src/app/lista-tareas/lista-tareas.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tarea } from '../interfaz/tareainterfaz'; 
+
+
 
 @Component({
   selector: 'app-lista-tareas',
   templateUrl: './lista-tareas.component.html',
   styleUrls: ['./lista-tareas.component.css']
 })
-export class ListaTareasComponent {
+export class ListaTareasComponent implements OnInit {
+  tarea: Tarea[] = [];
+  nuevaTarea: string = '';
 
+  ngOnInit() {
+    this.cargarLocalStorage();
+  }
+
+  agregarTarea() {
+    if (this.nuevaTarea.trim() !== '') {
+      const newTask: Tarea = { titulo: this.nuevaTarea.trim(), completed: false };
+      this.tarea.push(newTask);
+      this.guardarLocalStorage();
+      this.nuevaTarea = '';
+    }
+  }
+
+  cambioTarea(task: Tarea) {
+    this.guardarLocalStorage();
+  }
+
+  borrarTarea(task: Tarea) {
+    const index = this.tarea.indexOf(task);
+    if (index !== -1) {
+      this.tarea.splice(index, 1);
+      this.guardarLocalStorage();
+    }
+  }
+
+  guardarLocalStorage() {
+    localStorage.setItem('tarea', JSON.stringify(this.tarea));
+  }
+
+  cargarLocalStorage() {
+    const guardaTarea = localStorage.getItem('tarea');
+    if (guardaTarea) {
+      this.tarea = JSON.parse(guardaTarea);
+    }
+  }
 }
+
+
