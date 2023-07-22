fc591f3c8c21d8d777f07132ae2a83e17fd0f1c4
Brina <brixiga1892@gmail.com>
2023-07-21 23:25:42 -0500

    creando formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { ListaTareasComponent } from './lista-tareas/lista-tareas.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaTareasComponent,
+
   ],
   imports: [
     BrowserModule
